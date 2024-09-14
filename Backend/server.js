75b897c29327e307d16cdb7ce724f007ba21f05d
zameer <khanzameer5890@gmail.com>
2024-09-14 20:37:28 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,34 @@
+import  express  from "express";
+import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import {app,server} from  './socket/socket.js';
+
+// const app= express();
+const PORT=process.env.PORT||5000;
+
+dotenv.config();
+
+app.use(express.json());//to parse the from re.body
+app.use(cookieParser()); // to use cookies in req object
+
+// app.get("/",(req,res)=>{
+//     res.send("hello world!!");
+// });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+setTimeout(() => {
+    server.listen(PORT, () => {
+        connectToMongoDB();
+      console.log('Server is running on port '+PORT+" ");
+    });
+  }, 1000); // Wait for 1 second before starting the server
+  
